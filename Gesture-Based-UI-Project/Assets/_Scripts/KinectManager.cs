f1a4d625f65c32b667d4fd3a7ab37cd956cace67
KeithWilliamsGMIT <G00324844@gmit.ie>
2018-03-13 12:23:02 +0000

    Get a reference to the Kinect sensor.

--- Gesture-Based-UI-Project/Assets/_Scripts/KinectManager.cs
+++ Gesture-Based-UI-Project/Assets/_Scripts/KinectManager.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Windows.Kinect;
 using UnityEngine;
 
+/*
+ * This class is responsible for managing the Kinect sensor. There should only ever be one Kinect manager, therefore
+ * this class is a singleton. This class was adapted from:
+ * https://andreasassetti.wordpress.com/2015/11/02/develop-a-game-using-unity3d-with-microsoft-kinect-v2/
+ */
 public class KinectManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	private KinectSensor sensor;
+	private static KinectManager instance = null;
+
+	/*
+	 * Create a new instance of this class if none already exist.
+	 */
+	public void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy(gameObject);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	/*
+	 * Get a reference to the Kinect sensor.
+	 */
+	public void Start() {
+		sensor = KinectSensor.GetDefault();
+
+		if (sensor != null) {
+			Debug.Log("Kinect sensor available");
+		}
 	}
 }
