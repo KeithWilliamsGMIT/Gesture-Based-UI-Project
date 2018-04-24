bfd8eafb09c96bca2994eedab12358e18ff8989d
KeithWilliamsGMIT <G00324844@gmit.ie>
2018-04-24 10:12:12 +0100

    Added presentation slides.

--- Gesture-Based-UI-Project/Assets/_Scripts/GameManager.cs
+++ Gesture-Based-UI-Project/Assets/_Scripts/GameManager.cs
@@ -31,39 +31,41 @@ public class GameManager : MonoBehaviour {
 		ball.GetComponent<Rigidbody>().useGravity = false;
 		ball.enabled = false;
 
-        winText = GameObject.Find("WinText").GetComponent<Text>();
-        winBtn = GameObject.Find("WinButton");
-        winBtn.SetActive(false);
-        winText.enabled = false;
+        if (!isSinglePlayer) {
+            winText = GameObject.Find("WinText").GetComponent<Text>();
+            winBtn = GameObject.Find("WinButton");
+            winBtn.SetActive(false);
+            winText.enabled = false;
 
-        players = new List<PlayerEntity>();
-        PlayerEntity p1 = new PlayerEntity();
-        PlayerEntity p2 = new PlayerEntity();
+            players = new List<PlayerEntity>();
+            PlayerEntity p1 = new PlayerEntity();
+            PlayerEntity p2 = new PlayerEntity();
 
-        //Ready player 1
-        p1.setPlayerName(PlayerPrefs.GetString("Player0Name"));
-        p1.setPlayerScore(0);
-        players.Add(p1);
-        //Add second player
-        p2.setPlayerName(PlayerPrefs.GetString("Player1Name"));
-        p2.setPlayerScore(0);
-        players.Add(p2);
+            //Ready player 1
+            p1.setPlayerName(PlayerPrefs.GetString("Player0Name"));
+            p1.setPlayerScore(0);
+            players.Add(p1);
+            //Add second player
+            p2.setPlayerName(PlayerPrefs.GetString("Player1Name"));
+            p2.setPlayerScore(0);
+            players.Add(p2);
 
 
 
-        //Set player names from saved values
-        Text p1Text = GameObject.Find("Player1Text").GetComponent<Text>();
-        p1Text.text = players[0].getPlayerName();
-        p1Text.text += ": " + players[0].getPlayerScore();
+            //Set player names from saved values
+            Text p1Text = GameObject.Find("Player1Text").GetComponent<Text>();
+            p1Text.text = players[0].getPlayerName();
+            p1Text.text += ": " + players[0].getPlayerScore();
 
-        Text p2Text = GameObject.Find("Player2Text").GetComponent<Text>();
-        p2Text.text = players[1].getPlayerName();
-        p2Text.text += ": "+players[1].getPlayerScore();
+            Text p2Text = GameObject.Find("Player2Text").GetComponent<Text>();
+            p2Text.text = players[1].getPlayerName();
+            p2Text.text += ": "+players[1].getPlayerScore();
 
-        Debug.Log("New Scene! "+PlayerPrefs.GetString("Player0Name"));
-        Debug.Log(PlayerPrefs.GetString("Player1Name"));
-
-#if UNITY_EDITOR
+            Debug.Log("New Scene! "+PlayerPrefs.GetString("Player0Name"));
+            Debug.Log(PlayerPrefs.GetString("Player1Name"));
+        }
+        
+        #if UNITY_EDITOR
         Debug.Log("DEBUG: Press SPACE to serve the ball.");
 		#endif
 	}
@@ -130,23 +132,26 @@ public class GameManager : MonoBehaviour {
 		} else {
 			ball.SetDirection(-1);
             servingPlayer = PlayerEnum.PLAYER_ONE;
-            //Update player 1 score
-            int score = players[0].getPlayerScore();
-            players[0].setPlayerScore(score + 1);
-            
-            //Update player 1 score text
-            Text p1Text = GameObject.Find("Player1Text").GetComponent<Text>();
-            p1Text.text = players[0].getPlayerName();
-            p1Text.text += ": " + players[0].getPlayerScore();
 
-            //Check for Win condition
-            if (players[0].getPlayerScore() == 15)
-            {
-                //Set the win button to active
-                winBtn.SetActive(true);
-                //Set the win text
-                winText.enabled = true;
-                winText.text = players[0].getPlayerName() + " Wins!";
+            if (!isSinglePlayer) {
+                //Update player 1 score
+                int score = players[0].getPlayerScore();
+                players[0].setPlayerScore(score + 1);
+                
+                //Update player 1 score text
+                Text p1Text = GameObject.Find("Player1Text").GetComponent<Text>();
+                p1Text.text = players[0].getPlayerName();
+                p1Text.text += ": " + players[0].getPlayerScore();
+
+                //Check for Win condition
+                if (players[0].getPlayerScore() == 15)
+                {
+                    //Set the win button to active
+                    winBtn.SetActive(true);
+                    //Set the win text
+                    winText.enabled = true;
+                    winText.text = players[0].getPlayerName() + " Wins!";
+                }
             }
 
             ball.transform.position = ballInitialPosition;
